41072c74076b4d0308717f9ac732a7048250b421
Nirav Joshi <joshinirav8847@outlook.com>
2022-05-31 16:40:13 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+export default class App extends Component {
+  pageSize =15;
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+        <Navbar />
+          <Routes>
+            <Route path='/' exact element={<News key="general" pageSize={this.pageSize} country="in" category="general" />}></Route>
+            <Route path='/business' exact element={<News key="business" pageSize={this.pageSize} country="in" category="business" />}></Route>
+            <Route path='/entertainment' exact element={<News key="entertainment" pageSize={this.pageSize} country="in" category="entertainment" />}></Route>
+            <Route path='/health' exact element={<News key="health" pageSize={this.pageSize} country="in" category="health" />}></Route>
+            <Route path='/science' exact element={<News key="science" pageSize={this.pageSize} country="in" category="science" />}></Route>
+            <Route path='/sports' exact element={<News key="sports" pageSize={this.pageSize} country="in" category="sports" />}></Route>
+            <Route path='/technology' exact element={<News key="technology" pageSize={this.pageSize} country="in" category="technology" />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    )
+  }
 }
 
-export default App;
